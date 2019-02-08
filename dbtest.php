44a87d81a57b5c92bb6670c2fbe652d588078565
frnd4gan <43601157+frnd4gan@users.noreply.github.com>
2019-02-08 15:03:22 +0100

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,9 +1,9 @@
 <?php
-$dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("databaseuser");
-$dbpwd = getenv("databasepassword");
-$dbname = getenv("databasename");
+echo "<br> host".$dbhost = getenv("MYSQL_SERVICE_HOST");
+echo "<br> port".$dbport = getenv("MYSQL_SERVICE_PORT");
+echo "<br> User ".$dbuser = getenv("databaseuser");
+echo "<br>PWD ".$dbpwd = getenv("databasepassword");
+echo "<br>database ".$dbname = getenv("databasename");
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
