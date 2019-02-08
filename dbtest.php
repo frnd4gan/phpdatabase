f43be46675ba567ae7e8b9b24bfcb533c2ae53c1
frnd4gan <43601157+frnd4gan@users.noreply.github.com>
2019-02-08 15:09:28 +0100

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -4,7 +4,7 @@ echo "<br> port".$dbport = getenv("MYSQL_SERVICE_PORT");
 echo "<br> User ".$dbuser = "myuser";
 echo "<br>PWD ".$dbpwd = "mypassword";
 echo "<br>database ".$dbname = "mydatabase";
-die;
+
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
