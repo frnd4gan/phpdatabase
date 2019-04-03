f0a12616d3cf44e3491f5e83c2f70de8e23bd137
frnd4gan <43601157+frnd4gan@users.noreply.github.com>
2019-04-03 13:41:37 +0200

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -5,7 +5,7 @@ echo "<br> User is ".$dbuser = 'root_admin';//getenv("MYSQL_USER")."<br>";
 echo "<br> password is".$dbpwd = 'root_admin';//getenv("MYSQL_PASSWORD")."<br>";
 echo "<br>DBname is".$dbname = 'sampledb';getenv("MYSQL_DATABASE")."<br>";
 
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+$connection = mysql_connect($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
