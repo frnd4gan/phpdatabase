e3b17309db2b100a86f29e3ea6ab495aca9f37d6
frnd4gan <43601157+frnd4gan@users.noreply.github.com>
2019-04-03 15:58:35 +0200

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,15 +1,12 @@
 <?php
-$dbname = "sampledb";
-$servername = "localhost";
-$username = "root_admin";
-$password = "root_admin";
+try{
+$pdo = new PDO('mysql:host=localhost;dbname=sampledb;charset=utf8','root_admin','root_admin');
+$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+print "Connected to databse";
+}catch(PDOException $Exception) {
+die('Connection failed ー:'.$Exception->getMessage());
+}
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
 ?>
